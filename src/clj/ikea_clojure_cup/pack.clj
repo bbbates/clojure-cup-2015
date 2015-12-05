468a285f4e6e48b9e887e6d340eb00fcc7872e9b
Emile Raffoul <eraffoul@icm-consulting.com.au>
2015-12-05 19:31:44 +1100

    packit4me call

---
+++ src/clj/ikea_clojure_cup/pack.clj
@@ -0,0 +1,46 @@
+(ns ikea-clojure-cup.pack
+  (:require [org.httpkit.client :as http-kit]
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as cs]
+            [clojure.data.json :as json]))
+(comment
+  (def sample-param
+    {:bins [{:id "car" :depth 5 :weight 5 :height 5}]
+     :packages [{:id "11" :depth 10 :weight 10 :height 10}
+                {:id "22" :depth 20 :weight 2 :height 20}]}))
+
+(defn bin->string [{:keys [id depth weight height]}]
+  (format "%s:0:%sx%sx%s" id depth weight height))
+
+(defn package->string [{:keys [id depth weight height]}]
+  (format "%s:0:0:%sx%sx%s" id depth weight height))
+
+(defn pack [{:keys [bins packages]}]
+  (let [bins (cs/join "," (map bin->string bins))
+        items (cs/join "," (map package->string packages))
+        url (format "http://www.packit4me.com/api/call/raw?bins=%s&items=%s" bins items)
+        response (http-kit/post url)
+        packing-details (json/read-str (:body @response) :key-fn keyword)
+        requested-items-count (count packages)
+        items-count (-> packing-details first :items count)]
+    {:result (cond
+              (= items-count 0) :no
+              (= items-count requested-items-count) :yes
+              (not= items-count requested-items-count) :partial)
+     :details packing-details}))
+
+
+(comment
+  (pack sample-param)
+
+  (def res-yes [{:curr_weight 0, :weight_limit 0, :item_count 1, :size_2 5, :size "5 x 5 x 5", :size_1 5, :id "0", :size_3 5, :items [{:constraints 0, :y_origin_in_bin -1.5, :x_origin_in_bin -1.5, :sp_size_3 2, :size_2 2, :sp_size_2 2, :sp_size "2 x 2 x 2", :weight 0, :size_1 2, :orig_size "2 x 2 x 2", :id "22", :size_3 2, :z_origin_in_bin 1.5, :sp_size_1 2}]}])
+
+
+  (def resp-preview (let [response (http-kit/post "http://www.packit4me.com/api/call/preview?bins=0:50:5x5x5&items=0:0:15:1x1x1&binId=0")]
+    @response))
+
+  (def resp-yes (let [response (http-kit/post "http://www.packit4me.com/api/call/raw?bins=0:0:5x5x5&items=0:0:0:1x1x1,1:0:0:2x2x2")]
+    @response))
+
+  (def resp-no (let [response (http-kit/post "http://www.packit4me.com/api/call/raw?bins=0:0:5x5x5&items=0:0:0:10x10x10")]
+    @response)))
