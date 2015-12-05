52900f247bd9f62092e7f9fc1ddad39312b5537e
Emile Raffoul <eraffoul@icm-consulting.com.au>
2015-12-06 02:22:32 +1100

    fix partial calc

--- src/cljs/ikea_clojure_cup/pack_it.cljs
+++ src/cljs/ikea_clojure_cup/pack_it.cljs
@@ -16,9 +16,10 @@
   [result-state progress-fn]
   (fn [_ _]
     [:section
+     [:p (str @result-state)]
      (case (:result @result-state)
        :yes (heading "It'll fit!")
        :no (heading "It won't fit :(")
        :partial [:div
                  (heading "...kinda fits")
-                 [:p "It'll should fit better if you remove: " (cs/join "," (map #(-> % :id str) (:missing @result-state)))]])]))
+                 [:p "It'll should fit better if you remove: " (cs/join "," (map #(-> % :name str) (:missing @result-state)))]])]))
