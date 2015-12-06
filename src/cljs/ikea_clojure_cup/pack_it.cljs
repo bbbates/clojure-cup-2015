f113e3021c95c693172a2e3bf89d1bb84bc42371
hurzeler <hurzeler@icm-consulting.com.au>
2015-12-06 14:52:43 +1100

    Fixed viewport #main and button spacing

--- src/cljs/ikea_clojure_cup/pack_it.cljs
+++ src/cljs/ikea_clojure_cup/pack_it.cljs
@@ -24,24 +24,26 @@
        [:main.select-items-content
         [:div.search
          (case (:result result-state)
-          :yes [:div
-                [:img {:src "img/itFits.png"}]]
-          :no [:div
-               [:img {:src "img/itDoesNotFit.png"}]]
-          :partial [:div
-                    [:img {:src "img/itFitsMayBe.png"}]
-                    [:p "You'll have to remove or get creative with these products:"
-                     [:ul
-                      (map #(vector :li (-> % :name str)) (:missing result-state))]]])
+           :yes [:div
+                 [:img {:src "img/itFits.png"}]]
+           :no [:div
+                [:img {:src "img/itDoesNotFit.png"}]]
+           :partial [:div
+                     [:img {:src "img/itFitsMayBe.png"}]
+                     [:p "You'll have to remove or get creative with these products:"
+                      [:ul
+                       (map #(vector :li (-> % :name str)) (:missing result-state))]]])
          [:h3 "Trolley contents:"]
          [select-items/trolley-list-contents (reagent/cursor all-state [:trolley])]
-         [bootstrap/button {:bs-size :lg
-                            :bs-style :primary
-                            :on-click recalc-progress-fn} "Recalculate"]
-         [bootstrap/button {:title "Start over" :bs-style :danger
-                            :bs-size :lg
-                            :href "/"
-                            :on-click start-over-fn} "Start over"]
+
+         [bootstrap/button-toolbar
+          [bootstrap/button {:bs-size :lg
+                             :bs-style :primary
+                             :on-click recalc-progress-fn} "Recalculate"]
+          [bootstrap/button {:title "Start over" :bs-style :danger
+                             :bs-size :lg
+                             :href "/"
+                             :on-click start-over-fn} "Start over"]]
          [:h3 "Boot size:"]
          [:p (str (cs/join "cm x " (-> @all-state :fleet :vehicles first vals)) "cm")]
          [:h3 "Preview:"]
