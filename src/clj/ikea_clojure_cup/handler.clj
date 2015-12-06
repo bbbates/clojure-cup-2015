9f78f1880542292f84576aae272c78917f426423
Brendan Bates <batesbrendan@gmail.com>
2015-12-07 10:01:29 +1100

    Add google analytics

--- src/clj/ikea_clojure_cup/handler.clj
+++ src/clj/ikea_clojure_cup/handler.clj
@@ -29,7 +29,14 @@
      (include-css (if (env :dev) "css/ikea-helper.css" "css/ikea-helper.min.css"))]
     [:body
      mount-target
-     (include-js "js/app.js")]]))
+     (include-js "js/app.js")
+     [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-71085272-1', 'auto');
+      ga('send', 'pageview');"]]]))
 
 (def cards-page
   (html
