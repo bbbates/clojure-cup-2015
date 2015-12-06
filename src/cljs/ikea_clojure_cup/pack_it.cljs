fe3210fe9cc62aede43b6c4f04ea329c12874ef9
Emile Raffoul <eraffoul@icm-consulting.com.au>
2015-12-06 17:11:33 +1100

    update 3d view size

--- src/cljs/ikea_clojure_cup/pack_it.cljs
+++ src/cljs/ikea_clojure_cup/pack_it.cljs
@@ -51,5 +51,5 @@
            [:p "N/A"]
            [:div
             [:p.hidden-xs "In the below box, you can pan around and zoom in and out to see how to stack your packages."]
-            [:iframe.hidden-xs {:width "500" :height "500" :src (str "/ikea/preview?bins=" (-> result-state :preview :bins) "&items=" (-> result-state :preview :items))}]
+            [:iframe.hidden-xs {:width "555" :height "555" :src (str "/ikea/preview?bins=" (-> result-state :preview :bins) "&items=" (-> result-state :preview :items))}]
             [:p.visible-xs "3D preview showing how stack the packages is not available when viewing on small screen devices. Please try again on a larger screen."]])]]])))
